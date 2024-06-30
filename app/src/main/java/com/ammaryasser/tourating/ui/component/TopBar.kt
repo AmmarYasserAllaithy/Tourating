9cb8c5c9a28c8764721043882f43a4383e6e3e24
Ammar Yasser Allaithy <ammaryasserallaithy@gmail.com>
2024-06-30 12:43:59 +0300

    add TopBar component

---
+++ app/src/main/java/com/ammaryasser/tourating/ui/component/TopBar.kt
@@ -0,0 +1,46 @@
+package com.ammaryasser.tourating.ui.component
+
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.outlined.KeyboardArrowLeft
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme.colorScheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+import com.ammaryasser.tourating.R
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopBar(
+    title: String = stringResource(R.string.app_name),
+    navBack: Boolean = false,
+    onBack: () -> Unit = {},
+    actions: @Composable RowScope.() -> Unit = {},
+) {
+    TopAppBar(
+        title = {
+            Text(text = title)
+        },
+        actions = actions,
+        navigationIcon = {
+            if (navBack)
+                IconButton(onClick = onBack) {
+                    Icon(
+                        imageVector = Icons.AutoMirrored.Outlined.KeyboardArrowLeft,
+                        contentDescription = "",
+                        tint = colorScheme.onPrimary
+                    )
+                }
+        },
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = colorScheme.primary,
+            titleContentColor = colorScheme.onPrimary
+        )
+    )
+}
