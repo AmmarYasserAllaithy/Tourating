edeba44d4b72b2cb0aadc56a1b5343600b77d902
Ammar Yasser Allaithy <ammaryasserallaithy@gmail.com>
2024-07-04 04:45:32 +0300

    integrate constraint layout in details screen

--- app/src/main/java/com/ammaryasser/tourating/ui/screen/DetailsScreen.kt
+++ app/src/main/java/com/ammaryasser/tourating/ui/screen/DetailsScreen.kt
@@ -2,11 +2,15 @@ package com.ammaryasser.tourating.ui.screen
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.MaterialTheme.typography
 import androidx.compose.material3.Text
@@ -14,9 +18,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.layoutId
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.ammaryasser.tourating.R
 import com.ammaryasser.tourating.ui.component.RatingBar
@@ -44,71 +50,82 @@ fun DetailsScreen(
 ) {
     vm = viewModel(factory = DetailsScreenViewModel.Factory)
 
-    Column(modifier = Modifier.fillMaxSize()) {
+    BoxWithConstraints(Modifier.fillMaxSize()) {
 
-        TopBar(
-            title = stringResource(id = R.string.details),
-            navBack = true,
-            onNavBack = onNavBack
-        )
+        ConstraintLayout(
+            constraintSet = OrientationConstraints(minWidth),
+            modifier = Modifier.matchParentSize()
+        ) {
 
-        vm.tourating.observeAsState().value?.run {
+            TopBar(
+                modifier = Modifier.layoutId(Layouts.TopBar.id()),
+                title = stringResource(id = R.string.details),
+                navBack = true,
+                onNavBack = onNavBack
+            )
 
-            ReadonlyMap(latitude, longitude)
+            vm.tourating.observeAsState().value?.run {
 
-
-            Column(
-                modifier = Modifier
-                    .padding(24.dp)
-                    .fillMaxSize(),
-                verticalArrangement = Arrangement.spacedBy(appGap)
-            ) {
-
-                Text(
-                    text = siteName,
-                    style = typography.titleLarge,
-                    fontWeight = FontWeight.Bold,
-                    color = MaterialTheme.colorScheme.onBackground
-                )
-
-                Text(
-                    text = createdAt.formatTimestamp(),
-                    color = MaterialTheme.colorScheme.onBackground.copy(.5f)
+                ReadonlyMap(
+                    latitude,
+                    longitude,
+                    Modifier
+                        .clip(RoundedCornerShape(6.dp))
+                        .layoutId(Layouts.GMap.id())
                 )
 
                 Column(
                     modifier = Modifier
                         .fillMaxWidth()
-                        .clip(appRoundedShape)
-                        .background(MaterialTheme.colorScheme.surface)
-                        .padding(appGap),
+                        .verticalScroll(rememberScrollState())
+                        .layoutId(Layouts.FormColumn.id()),
                     verticalArrangement = Arrangement.spacedBy(appGap)
                 ) {
 
-                    RatingBar(rating = rating.toDouble())
-
                     Text(
-                        text = review,
+                        text = siteName,
+                        style = typography.titleLarge,
+                        fontWeight = FontWeight.Bold,
                         color = MaterialTheme.colorScheme.onBackground
                     )
 
+                    Text(
+                        text = createdAt.formatTimestamp(),
+                        color = MaterialTheme.colorScheme.onBackground.copy(.5f)
+                    )
+
+                    Column(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .clip(appRoundedShape)
+                            .background(MaterialTheme.colorScheme.surface)
+                            .padding(appGap),
+                        verticalArrangement = Arrangement.spacedBy(appGap)
+                    ) {
+
+                        RatingBar(rating = rating.toDouble())
+
+                        Text(
+                            text = review,
+                            color = MaterialTheme.colorScheme.onBackground
+                        )
+
+                    }
 
                 }
-
-
             }
+
+            id?.let { vm.fetchById(id) }
+
         }
-
-        id?.let { vm.fetchById(id) }
-
     }
 }
-
 
 @Composable
 fun ReadonlyMap(
     latitude: Double,
-    longitude: Double
+    longitude: Double,
+    modifier: Modifier = Modifier
 ) {
     val latLng = LatLng(latitude, longitude)
 
@@ -119,7 +136,7 @@ fun ReadonlyMap(
         uiSettings = MapUiSettings(
             zoomControlsEnabled = false
         ),
-        modifier = Modifier
+        modifier = modifier
             .fillMaxWidth()
             .heightIn(max = 333.dp)
     ) {
